29be805ce9c13f0a4ef91a652c314ce0c7cdedd6
iptoday <wangdong1221@outlook.com>
2024-07-11 21:26:49 +0800

    Introduce .idea directory and update project configuration files
    This commit adds the .idea directory along with several configuration files
    for the IDE, including changes to .gitignore, Dart Packages, and Flutter
    Plugins. It also updates the project's misc.xml, modules.xml, and
    vcs.xml files to reflect the current project structure and settings.

---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,18 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import desktop_drop
+import device_info_plus
+import package_info_plus
+import path_provider_foundation
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  DesktopDropPlugin.register(with: registry.registrar(forPlugin: "DesktopDropPlugin"))
+  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+}
